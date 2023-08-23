fea2cc4fdce2f35bc2036a1cec5e5c119e2cfb81
John Hembrom <thebytecomputer@outlook.com>
2023-08-23 14:07:59 +0530

    4.data types example codes added

--- 01-variables-data-types/04-data-types/script.js
+++ 01-variables-data-types/04-data-types/script.js
@@ -10,6 +10,9 @@ console.log(name,typeof name);
 const age =33;
 console.log(age, typeof age);
 
+const temp = 98.9;
+console.log(temp, typeof temp);
+
 const weight = 33.5;
 console.log(weight, typeof weight);
 
@@ -21,9 +24,48 @@ console.log(typeof isDeveloper);
 const bicycle = null;
 console.log(typeof bicycle);
 
+const aptNumber =  null;
+console.log(aptNumber, typeof aptNumber);
+//Mistake from the side of js maker, null in not an object
+
 let test;
 console.log(test);
 
-// REFERENCE TYPES- Objects
+// U N D E F I N E D
+let score;
+const output= score;
 
+console.log(output, typeof output);
+//Other way of doing the same thing
+const scored = undefined;
+console.log(scored, typeof scored);
+
+// S Y M B O L
+const id = Symbol('id');
+const result = id;
+console.log(result, typeof result);
+
+// B i g I n t
+const n = 2348278347812783748n;
+const reslt = n;
+console.log(reslt, typeof reslt);
+
+
+// R E F E R E N C E  TYPES- Objects
+// no.1 Arrays
+const numbers = [1,2,3,4,5]
+const outcome = numbers;
+console.log(numbers, typeof numbers);
+//no.2 Objects
+const person= {
+  name:'john',
+}
+const outpt = person;
+console.log(person, typeof outpt)
+//no.3 Functions
+function sayHello(){
+  console.log('Hello');
+}
+const otpt = sayHello;
+console.log(otpt, typeof otpt);
 //Array 
