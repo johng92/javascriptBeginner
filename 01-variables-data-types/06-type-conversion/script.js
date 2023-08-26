092e261bc97ce3ed5860e46062ad1d65a32f4cbc
John Hembrom <thebytecomputer@outlook.com>
2023-08-26 15:07:06 +0530

    06-type-conversion added

---
+++ 01-variables-data-types/06-type-conversion/script.js
@@ -0,0 +1,41 @@
+let amount = '100';
+
+//Convert:: S T R I N G - to - NUMBER
+// amount = parseInt(amount);
+// amount = +amount;
+amount = Number(amount);
+
+console.log(amount, typeof amount)
+
+//Convert::  N U M B E R - to - STRING
+
+let amounts = 200;
+// amounts = amounts.toString();     //toString() is method/function , which is attached(.) to an object(amounts)
+//JavaScript creates a temporary wrapper of the associated type.
+amounts = String(amounts);
+console.log(amounts, typeof amounts);
+
+
+// Convert:: S T R I N G - to - DECIMAL
+let weight = '58.5';
+weight = parseFloat(weight);
+console.log(weight, typeof weight);
+
+// Convert:: N U M B E R - to - BOOLEAN
+let Numb = 1;
+Numb = Boolean(Numb);
+
+console.log(Numb, typeof Numb)
+  /* if Numb = 0 , if give 0, its become falsy..*/
+
+/*Let's try string to boolean*/
+let zero = 'hello';
+zero = parseInt(zero);
+
+console.log(zero, typeof zero);
+  /*its give NaN== Not a number*, four other ways to get Nan */
+  console.log(Math.sqrt(-1));
+  console.log(1 + NaN);
+  console.log(undefined + undefined);
+  console.log('koo'/3);
+
