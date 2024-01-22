1888e56a934cc7679fd0399f3ba975a8b8cd1424
priyanka murmu <priyankamurmu769@gmail.com>
2024-01-22 19:03:32 +0530

    some comments & code added

--- 01-variables-data-types/11-numbers/app.js
+++ 01-variables-data-types/11-numbers/app.js
@@ -1,15 +1,14 @@
 let x;
 
 // const num = 500000;
-const numb = new Number (7) 
+const numb = new Number(7);
 /* console.log(typeof num);   gives - number/*
 console.log(typeof numb);     /*gives - object */
 
-
 const num = new Number(9);
 
-x = num.toString();  /* num== 9 , we are adding methods to existing number*/
-console.log(x, typeof x);   /*gives 9 string*/
+x = num.toString(); /* num== 9 , we are adding methods to existing number*/
+console.log(x, typeof x); /*gives 9 string*/
 
 // There is NO LENGTH PROPERTY for Numbers
 x = num.length;
@@ -17,23 +16,26 @@ console.log(x); /*gives- undefined*/
 
 //If u want to check how many digits are there, use .toString method
 x = num.toString().length;
-console.log(x, typeof x);  /*gives   1 , as above num=9 is there*/
+console.log(x, typeof x); /*gives   1 , as above num=9 is there, only 1 digit*/
 
 x = num.toFixed(2);
-console.log(x, typeof x);   /*gives 9.00 and it's string*/
+console.log(x, typeof x); /*gives 9.00 and it's string*/
 //If we take a number, then we use fixed on it, its going to turn into a STRING REPRESENTATION*/
 
 /* If we modify, numbers like this new Number(5.4538) We'll get 5.45 Bcoz, its fixed to two*/
 
-x = num.toPrecision(2); /*if its 9.5989 , it'll give 9.6 --gives result it Total 2 digits*/
-       /*if given, toPrecision(3), it'll give result in 3 digit*/
+// x =
+//   num.toPrecision(
+//     2
+//   ); /*if its 9.5989 , it'll give 9.6 --gives result it Total 2 digits*/
+/*if given, toPrecision(3), it'll give result in 3 digit*/
 console.log(x);
 
 x = num.toExponential(2);
 console.log(x); /*gives, 9.00e+0*/
 
 /*x = num.toLocaleString('en-US');*/
-x = num.toLocaleString('ar-EG') /*gives, local representation of Egyptian num of 9*/
+x = num.toLocaleString("ar-EG"); /*gives, local representation of Egyptian num of 9*/
 console.log(x);
 
 console.log(num);
@@ -46,4 +48,3 @@ x = Number.MAX_VALUE; /*gives 1.797...e+308 == 179 and 307zeroes*/
 x = Number.MIN_VALUE;
 
 console.log(x);
-
